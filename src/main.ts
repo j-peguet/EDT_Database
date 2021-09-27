ab94e15b54fe8e06097e92b77db1446197a633c4
Jules Peguet <julespeguet.jp@gmail.com>
2021-09-27 15:03:17 +0200

    init project (version + OpenAPI)

---
+++ src/main.ts
@@ -0,0 +1,24 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { VersioningType } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  
+  app.enableVersioning({
+    type: VersioningType.HEADER,
+    header: 'Version',
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('EDT_Database Documentation')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
+}
+bootstrap();
